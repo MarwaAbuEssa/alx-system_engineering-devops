7c6accaae5c7ed041d325656fa6393c835848bd3
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-23 00:54:43 -0800

    fix install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  require => Package['python3-pip'],
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  require => [Package['python3-pip'], Exec['install_werkzeug']],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
