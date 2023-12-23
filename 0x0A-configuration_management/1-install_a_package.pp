afa2fca853e738c6c64102c03968c902e273bed0
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-23 00:35:38 -0800

    fixing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-# Installs flask
+# install python pip
+package { 'python3-pip':
+  ensure => installed,
+}
 
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install flask
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  require => Package['python3-pip'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
