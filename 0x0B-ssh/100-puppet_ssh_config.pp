f17e0912c73212f4594fcdb82b770c4256092e45
MarwaAbuEssa <meromeroo@gmail.com>
2024-01-03 15:57:10 -0800

    fixing in puppert

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
-include stdlib
-file_line { 'Turn-off-passwd-auth':
-ensure  => 'present',
-path    => '/etc/ssh/ssh_config',
-line 	=> 'PasswordAuthentication no',
-match	=> 'PasswordAuthentication yes'
-}
+#!/usr/bin/env bash
+# use puppet to make chnages 
 
-file_line { 'Declare-identity-file':
-ensure  => 'present',
-path    => '/etc/ssh/ssh_config',
-line 	=> 'IdentityFile ~/.ssh/school',
-match	=> 'IdentityFile ~/.ssh/id_rsa'
+file { 'etc/ssh/ssh_config':
+	ensure  => present,
+content =>"
+	#ssh configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	"
 }
