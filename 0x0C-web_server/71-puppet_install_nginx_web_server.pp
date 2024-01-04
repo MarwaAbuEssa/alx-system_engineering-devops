5a9016891fb6f03281122804b51a22b006d08724
MarwaAbuEssa <meromeroo@gmail.com>
2024-01-03 18:06:48 -0800

    fix in puppet

---
+++ 0x0C-web_server/71-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+#config server
+
+package { 'nginx':
+  ensure => installed
+}
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/marwaabuessa permanant;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello Wolrd!',
+}
+service { 'nginx':
+  ensure => running,
+  require => package['nginx']
+}
