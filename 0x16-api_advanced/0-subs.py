3c78be48283756e784638fdd94a31c19ccdb3e23
MarwaAbuEssa <meromeroo@gmail.com>
2024-02-15 08:08:06 -0800

    fix in condition

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,12 @@ def number_of_subscribers(subreddit):
     """total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "api.advanced"
-            }
+        "User-Agent": "api.advanced"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
+    print(response.status_code)
     if response.status_code != 200:
         return 0
+
     results = response.json().get("data")
     return results.get("subscribers")
